7fc83cf97a9efbecede96a967fcf093ef3bb59db
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2025-07-14 13:38:09 +0530

    Time: 5 ms (67.27%), Space: 44.3 MB (36.91%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,9 +1,10 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        if(x < 0) return false;
         int n = x;
         int temp = 0;
 
-        while(n > 0) {
+        while(n != 0) {
             temp = temp*10 + n%10;
             n = n/10;
         }
