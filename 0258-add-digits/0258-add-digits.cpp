f6f6ce484d8f13235b3685832e780c1488125911
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-03-18 14:08:48 +0530

    Time: 0 ms (100%), Space: 8.6 MB (78.22%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int addDigits(int num) {
+       int sum = 0;
+
+        do {
+        sum = 0;
+        while(num > 0){
+            int dig = num%10;
+            sum += dig;
+            num = num/10;
+        }
+        num = sum;
+       }  while(num > 9);
+
+       return num;       
+    }
+};
