4c85b48ce07fc60cea476dcd75a8c915bff7cfd5
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-03-20 10:58:01 +0530

    Time: 0 ms (100%), Space: 28.3 MB (7.85%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,8 +1,26 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int mid = nums.size()/2;
-        return nums[mid];
+        // sort(nums.begin(), nums.end());
+        // int mid = nums.size()/2;
+        // return nums[mid];
+
+        //Moore's voting algorithm
+        int n = nums.size();
+        int freq = 0, ans = 0;
+
+        for(int i=0; i<n; i++) {
+            if(freq == 0) {
+                ans = nums[i];
+            }
+
+            if(nums[i] == ans) {
+                freq++;
+            } else {
+                freq--;
+            }
+        }
+
+        return ans;        
     }
 };
