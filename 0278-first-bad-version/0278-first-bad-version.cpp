cf94bafd1e1e4c45d600eb2a89180474f0c33c0c
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-03-22 13:45:03 +0530

    Time: 2 ms (56.73%), Space: 8 MB (4.95%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -7,23 +7,15 @@ public:
         int sv = 1;
         int ev = n;
 
-        if(n == 1){
-            if(isBadVersion(1)) return 1;
-            else return 0;
-        }
+        while(sv < ev) {
+            int mid = sv + (ev - sv) / 2;
 
-        while(sv <= ev) {
-            int mid = sv + (ev-sv)/2;
-            if(isBadVersion(mid) == false) sv = mid+1;
-            else {
-                int i = mid-1;
-                while(i >= 0) {
-                    if(isBadVersion(i) == false) return i+1;
-                    i--;
-                }
-
+            if(isBadVersion(mid)) {
+                ev = mid;        // possible answer, go left
+            } else {
+                sv = mid + 1;    // go right
             }
         }
-        return 0;
+        return sv;
     }
 };
