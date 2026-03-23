c36aaecaeab7987db75f261fd6812c562d12c8ab
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-03-23 12:14:39 +0530

    Time: 0 ms (100%), Space: 17.6 MB (54.14%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,35 +1,48 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ans;
+    int firstPos(vector<int> &nums, int target) {
         int n = nums.size();
         int si = 0;
         int ei = n-1;
+        int ans = -1;
 
         while(si <= ei) {
             int mid = si + (ei-si)/2;
-            if(nums[mid] < target) {
-                si = mid+1;
-            } else if(nums[mid] > target) {
+            if(nums[mid] > target) {
                 ei = mid-1;
+            } else if(nums[mid] < target) {
+                si = mid+1;
             } else {
-                int start = mid;
-                for(int i=mid-1; i>=0; i--) {
-                    if(nums[i] != target) break;
-                    else start = i;
-                }
-                int end = mid;
-                for(int i=mid+1; i<n; i++) {
-                    if(nums[i] != target) break;
-                    else end = i;
-                }
-                ans.push_back(start);
-                ans.push_back(end);
-                return ans;
+                ans = mid;
+                ei = mid-1;
             }
         }
-        ans.push_back(-1);
-        ans.push_back(-1);
+        return ans;
+    }
+    int lastPos(vector<int> &nums, int target) {
+        int n = nums.size();
+        int si = 0;
+        int ei = n-1;
+        int ans = -1;
+
+        while(si <= ei) {
+            int mid = si + (ei-si)/2;
+            if(nums[mid] > target) {
+                ei = mid-1;
+            } else if(nums[mid] < target) {
+                si = mid+1;
+            } else {
+                ans = mid;
+                si = mid+1;
+            }
+        }
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans;
+        int n = nums.size();
+        ans.push_back(firstPos(nums, target));
+        ans.push_back(lastPos(nums, target));        
         return ans;
     }
 };
