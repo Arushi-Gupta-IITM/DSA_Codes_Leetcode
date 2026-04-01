2bcc462ea2be012d3e370b749442044a7c4cfebd
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-04-01 09:49:53 +0530

    Time: 32 ms (28.27%), Space: 32.6 MB (10.92%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    bool isPermutationPresent(string s, vector<int> freq) {
-        int n = s.size();
+    bool isSameArray(vector<int> arr1, vector<int> arr2) {
+        int n = arr1.size();
         for(int i=0; i<n; i++) {
-            char ch = s[i];
-            freq[ch-'a']--;
-        }
-
-        for(int i=0; i<26; i++) {
-            if(freq[i] != 0) return false;
+            if(arr1[i] != arr2[i]) return false;
         }
         return true;
     }
     bool checkInclusion(string s1, string s2) {
-        int n1 = s1.size();
-        int n2 = s2.size();
+        int n1 = s1.length();
+        int n2 = s2.length();
+
+        if(n1 > n2) return false;
 
         vector<int> freq(26, 0);
         for(int i=0; i<n1; i++) {
@@ -22,10 +19,19 @@ public:
             freq[ch-'a']++;
         }
 
-        for(int i=0; i<(n2-n1+1); i++) {
-            string substring = s2.substr(i, n1);
-            if(isPermutationPresent(substring, freq)) return true;
+        vector<int> winFreq(26, 0);
+        for(int i=0; i<n1; i++) {
+            winFreq[s2[i]-'a']++;
         }
-        return false;
+
+        int in = n1;
+        while(in < n2) {
+            if(isSameArray(freq, winFreq)) return true;
+            char ch = s2[in];
+            winFreq[ch-'a']++;
+            winFreq[s2[in-n1]-'a']--;
+            in++;
+        }
+        return isSameArray(freq, winFreq);
     }
 };
