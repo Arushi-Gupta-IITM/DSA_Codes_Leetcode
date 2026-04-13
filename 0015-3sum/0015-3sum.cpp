8d825ec3fe45f24830a95d7079f6134a1f363164
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-04-13 11:38:50 +0530

    Time: 41 ms (88.93%), Space: 29.1 MB (45.15%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
         vector<vector<int>> ans;
-
+        int n = nums.size();
         sort(nums.begin(), nums.end());
 
         for(int i=0; i<n; i++) {
-            if(i > 0 && nums[i] == nums[i-1]) continue;
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int tar = -nums[i];
             int si = i+1, ei = n-1;
 
             while(si < ei) {
-                int sum = nums[i] + nums[si] + nums[ei];
-                if(sum == 0) {
-                    vector<int> list = {nums[i], nums[si], nums[ei]};
-                    ans.push_back(list);
-
-                    while(si < ei && nums[si] == nums[si+1]) si++;
-                    while(si < ei && nums[ei] == nums[ei-1]) ei--;
-
+                int sum = nums[si] + nums[ei];
+                if(sum < tar) si++;
+                else if(sum > tar) ei--;
+                else {
+                    ans.push_back({nums[i], nums[si], nums[ei]});
                     si++;
                     ei--;
-                } else if(sum > 0) ei--;
-                else si++;                
+
+                    while(si < ei && nums[si] == nums[si-1]) si++; // si or ei was already updated 
+                    while(si < ei && nums[ei] == nums[ei+1]) ei--;
+                }
             }
         }
 
