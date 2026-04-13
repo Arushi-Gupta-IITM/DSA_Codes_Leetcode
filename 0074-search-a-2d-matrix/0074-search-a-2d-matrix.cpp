021d6eaa70625e0e29b369c50d8e10024d6b2afd
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-04-13 11:55:35 +0530

    Time: 0 ms (100%), Space: 13.3 MB (78.41%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& mat, int tar) {
-        // optimal approach, dual binary search
-        int n = mat.size();
-        int m = mat[0].size();
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
 
-        int sr = 0, er = n-1;
-
+        int sr = 0, er = m-1;
         while(sr <= er) {
-            int midRow = sr + (er-sr)/2;
-            if(mat[midRow][0] <= tar && tar <= mat[midRow][m-1]) {
-                int si = 0;
-                int ei = m-1;
-                while(si <= ei) {
-                    int mid = si + (ei-si)/2;
-                    if(mat[midRow][mid] == tar) return true;
-                    if(tar > mat[midRow][mid]) si = mid+1;
-                    else ei = mid-1;
+            int midR = sr + (er-sr)/2;
+            if(matrix[midR][n-1] < target) sr = midR+1;
+            else if(matrix[midR][0] > target) er = midR-1;
+            else {
+                int sc = 0, ec = n-1;
+                while(sc <= ec) {
+                    int midC = sc + (ec-sc)/2;
+                    if(matrix[midR][midC] < target) sc = midC + 1;
+                    else if(matrix[midR][midC] > target) ec = midC - 1;
+                    else {
+                        return true;
+                    }
                 }
                 return false;
-            } else if(tar > mat[midRow][m-1]) sr = midRow+1;
-            else er = midRow-1;
+            }
         }
+
         return false;
     }
 };
