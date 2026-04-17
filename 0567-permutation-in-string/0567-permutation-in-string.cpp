54628b48c58720d9548b8bd392ffb94dcca2238a
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-04-17 14:04:56 +0530

    Time: 31 ms (27.1%), Space: 32.5 MB (10.58%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
-    bool isSameArray(vector<int> arr1, vector<int> arr2) {
-        int n = arr1.size();
-        for(int i=0; i<n; i++) {
-            if(arr1[i] != arr2[i]) return false;
+
+    bool isArrayEqual(vector<int> map1, vector<int> map2) {
+        for(int i=0; i<26; i++) {
+            if(map1[i] != map2[i]) return false;
         }
         return true;
     }
+
     bool checkInclusion(string s1, string s2) {
         int n1 = s1.length();
         int n2 = s2.length();
-
         if(n1 > n2) return false;
-
-        vector<int> freq(26, 0);
+        vector<int> map1(26, 0);
+        vector<int> map2(26, 0);
+        
         for(int i=0; i<n1; i++) {
-            char ch = s1[i];
-            freq[ch-'a']++;
+            map1[s1[i]-'a']++;
+            map2[s2[i]-'a']++;
         }
 
-        vector<int> winFreq(26, 0);
-        for(int i=0; i<n1; i++) {
-            winFreq[s2[i]-'a']++;
+        if(isArrayEqual(map1, map2)) return true;
+
+        for(int i=n1; i<n2; i++) {
+            map2[s2[i-n1]-'a']--;
+            map2[s2[i]-'a']++;
+
+            if(isArrayEqual(map1, map2)) return true;
         }
 
-        int in = n1;
-        while(in < n2) {
-            if(isSameArray(freq, winFreq)) return true;
-            char ch = s2[in];
-            winFreq[ch-'a']++;
-            winFreq[s2[in-n1]-'a']--;
-            in++;
-        }
-        return isSameArray(freq, winFreq);
+        return false;
     }
 };
