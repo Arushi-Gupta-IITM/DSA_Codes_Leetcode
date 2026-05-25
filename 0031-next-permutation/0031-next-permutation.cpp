ca184d1c0d9bdcccd80ff865b414081f43ca43b9
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-05-25 12:44:11 +0530

    Time: 0 ms (100%), Space: 15.7 MB (32.22%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -3,7 +3,7 @@ public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
         if(n == 0 || n == 1) return;
-        
+
         int pivot = -1;
         // find pivot, 
         for(int i=n-2; i>=0; i--) {
@@ -12,23 +12,26 @@ public:
                 break;
             }
         }
-        if(pivot == -1) { // array sorted in descending order
-            sort(nums.begin(), nums.end());
-            return;
-        }
+        
         // travel from back and find the element just greater than the pivot
-        int nextGreatest = 0;
-        for(int i=n-1; i>=0; i--) {
-            if(nums[i] > nums[pivot]) {
-                nextGreatest = i;
-                break;
+        if(pivot != -1) { // valid pivot, if pivot = -1, array in desc order
+            int nextGreatest = 0;
+            for(int i=n-1; i>=0; i--) {
+                if(nums[i] > nums[pivot]) {
+                    nextGreatest = i;
+                    break;
+                }
             }
+            // swap next greatest and pivot element
+            swap(nums[pivot], nums[nextGreatest]);
         }
 
-        // swap next greatest and pivot element
-        swap(nums[pivot], nums[nextGreatest]);
-
-        // sort element from pivot+1 till end in ascending order
-        sort(nums.begin()+pivot+1, nums.end());
+        // reverse the element from pivot+1 till end
+        int i = pivot+1, j = n-1;
+        while(i < j) {
+            swap(nums[i], nums[j]);
+            i++;
+            j--;
+        }
     }
 };
