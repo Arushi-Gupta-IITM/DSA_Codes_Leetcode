438ea46ab473efc184dff6df26606edaa91e67c3
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-05-26 11:06:17 +0530

    Time: 0 ms (100%), Space: 19.3 MB (86.96%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,63 +10,25 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {   
-        //Recursive approach: base case
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        // base case
         if(list1 == NULL) {
             return list2;
         }
+
         if(list2 == NULL) {
             return list1;
         }
 
-        if(list1->val <= list2->val) {
-            list1->next = mergeTwoLists(list1->next, list2);
-            return list1;
+        ListNode* head;
+        if(list1->val < list2->val) {
+            head = list1;
+            head->next = mergeTwoLists(list1->next, list2);
         } else {
-            list2->next = mergeTwoLists(list1, list2->next);
-            return list2;
+            head = list2;
+            head->next = mergeTwoLists(list1, list2->next);
         }
-        // Iterative approach     
-        // if(list1 == NULL) return list2;
-        // if(list2 == NULL) return list1;
 
-        // ListNode* temp;
-        // ListNode* head;
-
-        // if(list1->val <= list2->val) {
-        //     head = list1;
-        //     list1 = list1->next;
-        // } else {
-        //     head = list2;
-        //     list2 = list2->next;
-        // }
-        // temp = head;
-
-        // while(list1 != NULL && list2 != NULL) {
-        //     if(list1->val <= list2->val) {
-        //         temp->next = list1;
-        //         list1 = list1->next;
-        //     } else {
-        //         temp->next = list2;
-        //         list2 = list2->next;
-        //     }
-        //     temp = temp->next;
-        // }
-
-        // while(list1 != NULL) {
-        //     temp->next = list1;
-        //     list1 = list1->next;
-        //     temp = temp->next;
-        // }
-
-        // while(list2 != NULL) {
-        //     temp->next = list2;
-        //     list2 = list2->next;
-        //     temp = temp->next;
-        // }
-
-        // return head;
-
-    
+        return head;
     }
 };
