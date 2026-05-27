727b30c7f047781472b6ae28746d571a07995b3f
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-05-27 21:56:03 +0530

    Time: 4 ms (15.05%), Space: 10.2 MB (30.57%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
+    bool isAlphaNumeric(char ch) {
+        if(ch >= 'a' && ch <= 'z') return true;
+        if(ch >= 'A' && ch <= 'Z') return true;
+        if(ch >= '0' && ch <= '9') return true;
+
+        return false;
+    }
     bool isPalindrome(string s) {
-        string ans = "";
-        for(char ch : s) {
-            if(isalnum(ch)) {
-                ans += tolower(ch);
+        int n = s.length();
+        string str = "";
+
+        for(int i=0; i<n; i++) {
+            char ch = s[i];
+            if(isAlphaNumeric(ch)) {
+                str.push_back((char)tolower(ch));
             }
         }
 
-        int si = 0, ei = ans.length()-1;
-        while(si < ei) {
-            if(ans[si] != ans[ei]) return false;
-            si++;
-            ei--;
+        int i = 0, j = str.length()-1;
+
+        while(i < j) {
+            if(str[i] != str[j]) return false;
+            i++;
+            j--;
         }
         return true;
     }
