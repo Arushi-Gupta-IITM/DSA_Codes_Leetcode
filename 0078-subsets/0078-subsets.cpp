64b71337b82c9b8acb1a1f144eec455e70746529
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-05-28 10:17:39 +0530

    Time: 4 ms (7.45%), Space: 15.6 MB (13.7%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    void printSubsets(vector<int> &nums, vector<int> &subset, vector<vector<int>> &ans, int in) {
-        if(in == nums.size()) {
-            ans.push_back(subset); // values of subset vector are pushed back as a vector in ans
-            return;
-        }
-        //include
-        subset.push_back(nums[in]);
-        printSubsets(nums, subset, ans, in+1);
 
-        //backtrack
+    void subsetsUtil(vector<int> &nums, vector<int> subset, vector<vector<int>> &ans, int i) {
+        if(i == nums.size()) {
+            ans.push_back(subset);
+            return;            
+        }
+
+        subset.push_back(nums[i]);
+        subsetsUtil(nums, subset, ans, i+1);
         subset.pop_back();
-        //exclude
-        printSubsets(nums, subset, ans, in+1);
+        subsetsUtil(nums, subset, ans, i+1);
     }
+
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> subset;
-        printSubsets(nums, subset, ans, 0);
+
+        subsetsUtil(nums, subset, ans, 0);
         return ans;
     }
 };
