27cd41c5f4bec6be0ece15e0083c164d5f9db1e6
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-06-11 13:06:44 +0530

    Time: 231 ms (10.65%), Space: 38.5 MB (10.4%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
         int n = nums.size();
         sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;      
+        vector<int> trip(3);
+        set<vector<int>> st;
+        int tar;
 
         for(int i=0; i<n; i++) {
-            if(i>0 && nums[i] == nums[i-1]) continue;
-            int tar = -nums[i];
+            tar = -nums[i];
             int si = i+1, ei = n-1;
-
             while(si < ei) {
                 int sum = nums[si] + nums[ei];
-                if(sum < tar) si++;
-                else if(sum > tar) ei--;
-                else {
-                    ans.push_back({nums[i], nums[si], nums[ei]});
+                if(sum == tar) {
+                    trip[0] = nums[i];
+                    trip[1] = nums[si];
+                    trip[2] = nums[ei];
+                    st.insert(trip);
                     si++;
                     ei--;
-
-                    while(si < ei && nums[si] == nums[si-1]) si++; // si or ei was already updated 
-                    while(si < ei && nums[ei] == nums[ei+1]) ei--;
+                } else if(sum < tar) {
+                    si++;
+                } else {
+                    ei--;
                 }
-            }
+            }      
+            
+        }
+
+        for(auto triplet : st) {
+            ans.push_back(triplet);
         }
 
         return ans;
