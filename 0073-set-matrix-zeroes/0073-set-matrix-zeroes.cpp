8e47d0e1aa9703e72caf2b5b64f6e47f2d0ae7c5
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-06-13 13:26:24 +0530

    Time: 0 ms (100%), Space: 20.8 MB (75.95%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,61 +1,62 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        //using first row and first col to store which of them should be set to 0
-        int m = matrix.size();
-        int n = matrix[0].size();
+    void setZeroes(vector<vector<int>>& mat) {
+        int m = mat.size();
+        int n = mat[0].size();
 
-        bool firstRow0 = false;
-        bool firstCol0 = false;
+        bool isFirstRow0 = false;
+        bool isFirstCol0 = false;
 
         for(int j=0; j<n; j++) {
-            if(matrix[0][j] == 0) {
-                firstRow0 = true;
+            if(mat[0][j] == 0) {
+                isFirstRow0 = true;
                 break;
             }
         }
 
         for(int i=0; i<m; i++) {
-            if(matrix[i][0] == 0) {
-                firstCol0 = true;
+            if(mat[i][0] == 0) {
+                isFirstCol0 = true;
                 break;
             }
         }
 
         for(int i=1; i<m; i++) {
             for(int j=1; j<n; j++) {
-                if(matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+                if(mat[i][j] == 0) {
+                    mat[i][0] = 0;
+                    mat[0][j] = 0;
                 }
             }
         }
 
-        for(int i=1; i<m; i++) {
-            if(matrix[i][0] == 0) {
-                for(int j=1; j<n; j++) {
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-
+        // moving along first row, setting columns 0
         for(int j=1; j<n; j++) {
-            if(matrix[0][j] == 0) {
+            if(mat[0][j] == 0) {
                 for(int i=1; i<m; i++) {
-                    matrix[i][j] = 0;
+                    mat[i][j] = 0;
+                }
+            }
+        }
+        // moving along first col, setting rows 0
+        for(int i=1; i<m; i++) {
+            if(mat[i][0] == 0) {
+                for(int j=1; j<n; j++) {
+                    mat[i][j] = 0;
                 }
             }
         }
 
-        if(firstRow0) {
+        // first row 0
+        if(isFirstRow0) {
             for(int j=0; j<n; j++) {
-                matrix[0][j] = 0;
+                mat[0][j] = 0;
             }
         }
-
-        if(firstCol0) {
+        // first col 0
+        if(isFirstCol0) {
             for(int i=0; i<m; i++) {
-                matrix[i][0] = 0;
+                mat[i][0] = 0;
             }
         }
     }
