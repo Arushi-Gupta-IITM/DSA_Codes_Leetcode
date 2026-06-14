878c5fa0048ac8c7865d22610811bbdbe7faf8df
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-06-14 11:32:41 +0530

    Time: 0 ms (100%), Space: 14.1 MB (74.24%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    void combinationSumUtil(vector<int>& arr, vector<vector<int>> &ans, vector<int> &comb, int i, int tar) {        
+    void combinationSumUtil(vector<int> &arr, vector<vector<int>> &ans, vector<int> &comb, int tar, int i) {
+        // base case
         if(tar == 0) {
             ans.push_back(comb);
             return;
         }
 
-        if(i < 0) {
-            return;
-        }
-        
-        
-        if(arr[i] <= tar) {
+        if(i == arr.size()) return;
+
+        if(arr[i] <= tar)  { // given arr[i] element are positive
             // include current element
             comb.push_back(arr[i]);
-            combinationSumUtil(arr, ans, comb, i, tar-arr[i]);
+            combinationSumUtil(arr, ans, comb, tar-arr[i], i);
+            comb.pop_back();
+        } 
 
-            //backtrack
-            comb.pop_back();            
-        }
-        
         // exclude current element
-        combinationSumUtil(arr, ans, comb, i-1, tar);
+        combinationSumUtil(arr, ans, comb, tar, i+1); 
     }
     vector<vector<int>> combinationSum(vector<int>& arr, int target) {
         vector<vector<int>> ans;
         vector<int> comb;
-        int n = arr.size();
-        combinationSumUtil(arr, ans, comb, n-1, target);
+        combinationSumUtil(arr, ans, comb, target, 0);
         return ans;
     }
 };
