41087712c3bb5f5c81e940c2d24c86d9b38a3cb7
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-07-02 12:33:50 +0530

    Time: 0 ms (100%), Space: 25.9 MB (78.27%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        int n = height.size();
-        vector<int> lb(n);
-        vector<int> rb(n);
+    int trap(vector<int>& h) {
+        int n = h.size();
+        int l = 0, r = n - 1;
+        int lmax = 0, rmax = 0;
 
-        // filling left boundry values
-        lb[0] = 0;
-        for(int i=1; i<n; i++) {
-            lb[i] = max(lb[i-1], height[i-1]);
-        }
-        // filling right boundry values
-        rb[n-1] = 0;
-        for(int i=n-2; i>=0; i--) {
-            rb[i] = max(rb[i+1], height[i+1]);
-        }
-        // calculating ans  
-        int curr = 0;
         int ans = 0;
+        while (l <= r) {
+            lmax = max(h[l], lmax);
+            rmax = max(h[r], rmax);
 
-        for(int i=0; i<n; i++) {
-            curr = max(0, min(lb[i], rb[i])-height[i]);
-            ans += curr;
+            if (lmax < rmax) {
+                ans += lmax - h[l];
+                l++;
+            } else {
+                ans += rmax - h[r];
+                r--;
+            }
         }
-
         return ans;
     }
 };
