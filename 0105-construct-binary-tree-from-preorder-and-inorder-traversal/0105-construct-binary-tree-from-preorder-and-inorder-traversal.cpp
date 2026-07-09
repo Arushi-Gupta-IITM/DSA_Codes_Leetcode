902b0e0e54c8149b482b42f49afc3fbe3dc085e6
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-07-09 11:30:44 +0530

    Time: 2 ms (78.1%), Space: 27.5 MB (33.73%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,31 +11,26 @@
  */
 class Solution {
 public:
-
     TreeNode* buildTreeUtil(vector<int>& preorder, vector<int>& inorder, int &idx, int si, int ei, unordered_map<int, int> &mp) {
         if(si > ei) return NULL;
+        
         int rootIdx = mp[preorder[idx]];
         TreeNode* root = new TreeNode(preorder[idx]);
         idx++;
-
+                
         root->left = buildTreeUtil(preorder, inorder, idx, si, rootIdx-1, mp);
         root->right = buildTreeUtil(preorder, inorder, idx, rootIdx+1, ei, mp);
 
-        return root;
+        return root; 
     }
-
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        // idx: works on preorder
-        // si and ei work on inorder
-        int si = 0, ei = inorder.size()-1;
-        int idx = 0;
-
-        unordered_map<int, int> mp; // stores inorder in a hashmap: data : index to find root index
-
+        unordered_map<int, int> mp; // map of inorder, element: index
         for(int i=0; i<inorder.size(); i++) {
-            mp[inorder[i]] = i; // element : index
+            mp[inorder[i]] = i;
         }
-
+        
+        int si = 0, ei = preorder.size()-1;
+        int idx = 0;
         return buildTreeUtil(preorder, inorder, idx, si, ei, mp);
     }
 };
