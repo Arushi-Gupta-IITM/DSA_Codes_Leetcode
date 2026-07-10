28bc0893a92d404344d18919ab8e0dd62c645efb
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-07-10 11:28:45 +0530

    Time: 0 ms (100%), Space: 8.1 MB (16.44%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
+    int fibM(int n, vector<int> &dp) {
+        if(n <= 1) {
+            dp[n] = n;
+            return dp[n];
+        }
+
+        if(dp[n] == -1) {
+            dp[n] = fibM(n-1, dp) + fibM(n-2, dp);
+        }
+        return dp[n];
+    }
     int fib(int n) {
-        if(n == 0 || n == 1) return n;
-        return fib(n-1) + fib(n-2);
+        vector<int> dp(n+1, -1);
+        return fibM(n, dp);
     }
 };
