80af9de201444808415a289a388f8f4cceb84cb3
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-07-13 21:04:07 +0530

    Time: 0 ms (100%), Space: 10 MB (97.61%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -6,13 +6,17 @@ public:
         if(n == 1) return nums[0];
         if(n == 2) return max(nums[0], nums[1]);
 
-        vector<int> dp(n, 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        int prev2 = nums[0];
+        int prev1 = max(nums[0], nums[1]);
+
+        int ans = 0;
 
         for(int i=2; i<n; i++) {
-            dp[i] = max(dp[i-1], dp[i-2]+nums[i]);
+            ans = max(prev2+nums[i], prev1);
+            prev2 = prev1;
+            prev1 = ans;
         }
-        return dp[n-1];
+
+        return ans;
     }
 };
