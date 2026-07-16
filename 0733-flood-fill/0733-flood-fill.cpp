94ef1b81c55e7d9c606c05591e4f48cbbc44a375
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-07-16 12:08:30 +0530

    Time: 0 ms (100%), Space: 18.2 MB (58.35%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void floodFillUtil(vector<vector<int>>& image, vector<vector<bool>>& vis, int &org, int &color, int i, int j) {
+        int n = image.size();
+        int m = image[0].size();
+        if(i < 0 || j < 0) return;
+        if(i >= n || j >= m) return;
+        if(vis[i][j]) return;
+        if(image[i][j] != org) return;
+
+        vis[i][j] = true;
+        org = image[i][j];
+        image[i][j] = color;
+
+        // call for neighbours
+        floodFillUtil(image, vis, org, color, i-1, j);
+        floodFillUtil(image, vis, org, color, i+1, j);
+        floodFillUtil(image, vis, org, color, i, j-1);
+        floodFillUtil(image, vis, org, color, i, j+1);
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int org = image[sr][sc]; // n*m grid
+        int n = image.size();
+        int m = image[0].size();
+
+        vector<vector<bool>> vis(n, vector<bool>(m, false));
+        floodFillUtil(image, vis, org, color, sr, sc);
+
+        return image;
+    }
+};
