4c2ae88a27f6d9aaaaa05ee5499ca78f62f5b56a
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-07-22 10:43:58 +0530

    Time: 0 ms (100%), Space: 17.7 MB (29.42%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -3,6 +3,7 @@ public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
         vector<int> dp(n+1, 0);
+        dp[n] = 0;
         dp[n-1] = cost[n-1];
 
         for(int i=n-2; i>=0; i--) {
