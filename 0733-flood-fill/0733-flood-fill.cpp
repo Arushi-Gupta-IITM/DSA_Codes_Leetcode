270cbe6a5872d16c65598c98d61be390ee731a8d
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-07-23 10:27:27 +0530

    Time: 0 ms (100%), Space: 18.1 MB (57.93%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    void floodFillUtil(vector<vector<int>>& image, vector<vector<bool>>& vis, int &org, int &color, int i, int j) {
-        int n = image.size();
-        int m = image[0].size();
-        if(i < 0 || j < 0) return;
-        if(i >= n || j >= m) return;
-        if(vis[i][j]) return;
-        if(image[i][j] != org) return;
+    void floodFillUtil(vector<vector<int>>& image, int row, int col, int color, int n, int m, int orgColor) {
+        image[row][col] = color;
 
-        vis[i][j] = true;
-        image[i][j] = color;
-
-        // call for neighbours
-        floodFillUtil(image, vis, org, color, i-1, j);
-        floodFillUtil(image, vis, org, color, i+1, j);
-        floodFillUtil(image, vis, org, color, i, j-1);
-        floodFillUtil(image, vis, org, color, i, j+1);
+        if(row-1 >= 0 && image[row-1][col] == orgColor) floodFillUtil(image, row-1, col, color, n, m, orgColor);
+        if(row+1 < n && image[row+1][col] == orgColor) floodFillUtil(image, row+1, col, color, n, m, orgColor);
+        if(col-1 >= 0 && image[row][col-1] == orgColor) floodFillUtil(image, row, col-1, color, n, m, orgColor);
+        if(col+1 < m && image[row][col+1] == orgColor) floodFillUtil(image, row, col+1, color, n, m, orgColor);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int org = image[sr][sc]; // n*m grid
+        // n * m grid
         int n = image.size();
         int m = image[0].size();
-
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
-        floodFillUtil(image, vis, org, color, sr, sc);
+        if (image[sr][sc] == color) return image;
+        floodFillUtil(image, sr, sc, color, n, m, image[sr][sc]);
 
         return image;
     }
