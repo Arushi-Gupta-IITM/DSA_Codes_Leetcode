217d64303559388aef52b2adbfcc9bacc75922eb
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-08-03 08:05:44 +0530

    Time: 1 ms (35.5%), Space: 97.2 MB (88.14%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -3,18 +3,15 @@ public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         int buyPrice = prices[0];
-        int profit = 0;
-        int maxProfit = 0;
+        int ans = 0;
 
         for(int i=1; i<n; i++) {
-            if(prices[i] > buyPrice) {
-                profit = prices[i] - buyPrice;
-                maxProfit = max(profit, maxProfit);
-            } else {
+            if(prices[i] < buyPrice) {
                 buyPrice = prices[i];
+            } else {
+                ans = max(ans, prices[i]-buyPrice);
             }
         }
-
-        return maxProfit;
+        return ans;
     }
 };
