b9429fad577265e940f8e866b41e55a74b23386b
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-08-07 21:07:52 +0530

    Time: 0 ms (100%), Space: 10.8 MB (24.34%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
+    int robUtil(vector<int> &nums, vector<int> &dp, int i) {
+        if(i == 0) {
+            dp[i] = nums[0];
+            return dp[i];
+        }
+
+        if(i == 1) {
+            dp[i] = max(nums[0], nums[1]);
+            return dp[i];
+        }
+
+        if(dp[i] != -1) return dp[i];
+
+        dp[i] = max(robUtil(nums, dp, i-1), nums[i]+robUtil(nums, dp, i-2));
+        return dp[i];
+    }
     int rob(vector<int>& nums) {
         // dp[i]: max amount robbed till house i
         int n = nums.size();
         if(n == 1) return nums[0];
-
-        // memory optimization
-        int first = nums[0];
-        int sec = max(nums[0], nums[1]);
-        int ans = sec;
-
-        for(int i=2; i<n; i++) {
-            ans = max(sec, first + nums[i]);
-
-            first = sec;
-            sec = ans;
-        }
-        return ans;
+        vector<int> dp(n, -1);
+        // memoization
+        return robUtil(nums, dp, n-1);
     }
 };
