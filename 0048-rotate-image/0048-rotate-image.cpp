9a5abc267e12b12567d5ee75019ad59f53358fa5
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-08-07 08:56:36 +0530

    Time: 0 ms (100%), Space: 10.3 MB (38.19%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& mat) {
-        // standard logic: transpose of matrix in palce and then reverse the rows, n*m matrix
+        // take transpose and reverse rows
         int n = mat.size();
-        int m = mat[0].size();
-
-        // transpose of matrix in place
         for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(i > j) {
-                    swap(mat[i][j], mat[j][i]);
-                }
+            for(int j=0; j<n; j++) {
+                if(i < j) swap(mat[i][j], mat[j][i]);
             }
         }
 
-        // reverse each row
         for(int i=0; i<n; i++) {
-            int si = 0, ei = m-1;
-            while(si < ei) {
-                swap(mat[i][si++], mat[i][ei--]);
-            }
+            int sc = 0, ec = n-1;
+            while(sc < ec) swap(mat[i][sc++], mat[i][ec--]);
         }
     }
 };
