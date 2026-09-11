8ed0cdd4b7344256e11376e522a55c045eba1b63
Arushi Gupta <142425728+Arushi-Gupta-IITM@users.noreply.github.com>
2026-09-11 22:08:03 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.39%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -3,10 +3,11 @@ public:
     int mySqrt(int x) {
         int si = 0;
         int ei = x;
-
+        
+        // binary search on the answer space to find sqrt
         while(si <= ei) {
             int mid = si + (ei-si)/2;
-            long long sq = 1LL * mid * mid;
+            long long int sq = 1LL * mid * mid;
             if(sq == x) return mid;
             else if(sq > x) ei = mid-1;
             else si = mid+1;
